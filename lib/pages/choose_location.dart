39c79eb1d779cd50390d6a857926ada683c860a3
orji <ndubuisiaso@gmail.com>
2022-03-05 16:45:58 +0100

    finished tutorial

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -5,13 +5,13 @@ class ChooseLocation extends StatefulWidget {
   @override
   _ChooseLocationState createState() => _ChooseLocationState();
 }
-
+class _ChooseLocationState extends State<ChooseLocation> {
 List<WorldTime> locations = [
   WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.jpg'),
-  WorldTime(url: 'Africa/Lagos', location: 'Nigeria', flag: 'nigeria.jpg'),
-  WorldTime(url: 'America/New York', location: 'New York', flag: 'us.jpg'),
+  WorldTime(url: 'Africa/Lagos', location: 'Lagos', flag: 'nigeria.jpg'),
+  WorldTime(url: 'America/New_York', location: 'New York', flag: 'us.jpg'),
   WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.jpg'),
-  WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.jpg'),
+  WorldTime(url: 'Pacific/Niue', location: 'Pacific', flag: 'kenya.jpg'),
   WorldTime(url: 'Europe/Berlin', location: 'Berlin ', flag: 'germany.jpg'),
 ];
 
@@ -19,15 +19,13 @@ void updateTime(index) async {
   WorldTime instance = locations[index];
   await instance.getTime();
   //navigate to home screen
-  // Navigator.pop(context, {
-  //   'location': instance.location,
-  //   'flag': instance.flag,
-  //   'time': instance.time,
-  //   'isDaytime': instance.isDaytime
-  // });
+   Navigator.pop(context, {
+    'location': instance.location,
+    'flag':    instance.flag,
+    'time': instance.time,
+    'isDaytime': instance.isDaytime,
+  });
 }
-
-class _ChooseLocationState extends State<ChooseLocation> {
 
   @override
   Widget build(BuildContext context) {
@@ -35,18 +33,17 @@ class _ChooseLocationState extends State<ChooseLocation> {
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
-        title: Text('Choose a Locatiofn'),
+        title: Text('Choose a Location'),
         centerTitle: true,
         elevation: 0,
       ),body: ListView.builder(
-      itemCount: locations.length,
-      itemBuilder: (context, index) {
+      itemCount: locations.length, itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
           child: Card(
             child: ListTile(
               onTap: (){
-                updateTime(index);
+             updateTime(index);
               },
               title: Text(locations[index].location),
               leading: CircleAvatar(
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -3,16 +3,19 @@ import 'package:world_time/services/world_time.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
-
   @override
   _LoadingState createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
 
+  void miller() async {
+    Navigator.pop(context, true);
+  }
+
   void setupWorldTime() async {
     WorldTime instance = WorldTime(
-        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+        location: 'Lagos', flag: 'nigeria.jpg', url: 'Africa/Lagos');
     await instance.getTime();
     Navigator.pushReplacementNamed(context, '/home', arguments: {
       'location': instance.location,
@@ -31,7 +34,7 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    var data;
+   // var data;
     return Scaffold(
       backgroundColor: Colors.blue[900],
         body: Center (
@@ -43,5 +46,4 @@ class _LoadingState extends State<Loading> {
     );
   }
 }
-
 
